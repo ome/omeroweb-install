c5a5f2b56109fbc0fe80c84d6e2bb3851f16b44c
Ola Tarkowska <A.Tarkowska@dundee.ac.uk>
2016-08-29 11:03:32 +0100

    update templates and fix travis

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -6,11 +6,12 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     sudo DEBIAN_FRONTEND=noninteractive apt-get install -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" docker-engine
     sudo apt-get -y install shunit2
 
+    pip install -r requirements.txt
+
 else
 
     brew update
     sudo easy_install pip
+    sudo pip install -r requirements.txt
 
 fi
-
-pip install -r requirements.txt
--- test/docker-build.sh
+++ test/docker-build.sh
@@ -3,6 +3,11 @@
 set -e -u -x
 
 OS=${OS:-centos7}
+ICEVER=${ICEVER:-3.6}
+OMEROVER=${OMEROVER:-latest}
+WEBPORT=${WEBPORT:-}
+WEBPREFIX=${WEBPREFIX:-}
+WEBSESSION=${WEBSESSION:-}
 
 CACHE=${CACHE:---no-cache}
 
@@ -17,4 +22,10 @@ rm -rf $path/omeroweb-install-test
 IMAGE=omeroweb_install_test_$OS
 echo "Building image $IMAGE"
 
-docker build -t ${IMAGE} ${CACHE} $path -f $path/Dockerfile_$OS
+docker build -t ${IMAGE} \
+    --build-arg ICEVER=${ICEVER} \
+    --build-arg OMEROVER=${OMEROVER} \
+    --build-arg WEBPORT=${WEBPORT} \
+    --build-arg WEBPREFIX=${WEBPREFIX} \
+    --build-arg WEBSESSION=${WEBSESSION} \
+    ${CACHE} $path -f $path/Dockerfile_$OS
