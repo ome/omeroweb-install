7068f255910ea6a4adad9d952190f7c16b4f4e37
Ola Tarkowska <A.Tarkowska@dundee.ac.uk>
2016-08-31 10:26:28 +0100

    run ansible playbook directly

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -21,4 +21,45 @@ else
 
 fi
 
-./build --os=${OS} --omero-version=${OMEROVER:-} --ice-version=${ICEVER:-} web-prefix=${WEBPREFIX:-} web-port=${WEBPORT:-} web-server-conf=${WEBSERVER_CONF:-} web-server-name=${WEBSERVER_NAME:-}
+# process env vars
+# script used from https://gist.github.com/Jimilian/c3a2d8bb6df1b8ca64d02a10d97f510b
+arr=();
+
+arr=("os" "${OS}")
+arr=(${arr[@]} "ice_version" "${ICEVER}")
+if [[ ${WEBPREFIX:-} = *[!\ ]* ]]; then
+    arr=(${arr[@]} "web_prefix" "${WEBPREFIX:-}")
+fi
+if [[ ${WEBPORT:-} = *[!\ ]* ]]; then
+    arr=(${arr[@]} "web_port" "${WEBPORT:-}")
+fi
+if [[ ${WEBSERVER_CONF:-} = *[!\ ]* ]]; then
+    arr=(${arr[@]} "web_server_conf" "${WEBSERVER_CONF:-}")
+fi
+if [[ ${WEBSERVER_NAME:-} = *[!\ ]* ]]; then
+    arr=(${arr[@]} "web_server_name" "${WEBSERVER_NAME:-}")
+fi
+if [ "${ICEVER}" == "3.5" ]; then
+    arr=(${arr[@]} "system_site_packages" "True")
+fi
+
+
+vars=(${arr[@]})
+len=${#arr[@]}
+
+extravars="$(printf "{"
+for (( i=0; i<len; i+=2 ))
+do
+    printf "\"${vars[i]}\": \"${vars[i+1]}\""
+    if [ $i -lt $((len-2)) ] ; then
+        printf ", "
+    fi
+done
+printf "}"
+echo)"
+
+path=`dirname $0`/../
+
+# build
+ansible-playbook $path/ansible/omeroweb-install.yml -i $path/ansible/hosts/${OS}-ice${ICEVER} --extra-vars "${extravars}"
+ansible-playbook $path/ansible/omeroweb-install-doc.yml -i $path/ansible/hosts/${OS}-ice${ICEVER} --extra-vars "${extravars}"
