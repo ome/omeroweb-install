a29f9545d6e24096d9c4daef87d0014f78fe6fdb
Jean-Marie Burel <j.burel@dundee.ac.uk>
2017-07-07 14:14:43 +0200

    Add space

--- ansible/debian-ice3.6/websession_configure.sh
+++ ansible/debian-ice3.6/websession_configure.sh
@@ -10,4 +10,4 @@ set -u
 pip install --upgrade -r /home/omero/OMERO.py/share/web/requirements-redis.txt
 
 /home/omero/OMERO.py/bin/omero config set omero.web.session_engine 'django.contrib.sessions.backends.cache'
-/home/omero/OMERO.py/bin/omero config set omero.web.caches '{"default": {"BACKEND": "django_redis.cache.RedisCache","LOCATION": "redis://127.0.0.1:6379/0"}}'
+/home/omero/OMERO.py/bin/omero config set omero.web.caches '{"default": {"BACKEND": "django_redis.cache.RedisCache", "LOCATION": "redis://127.0.0.1:6379/0"}}'
